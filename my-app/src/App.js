da1974d7d6b7c42054e15dc329c0965de50e0dac
Mary Baker <meb601@gmail.com>
2020-09-01 16:55:49 -0400

    Add Github Octocat logo and install react app

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
